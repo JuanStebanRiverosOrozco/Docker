8dcd475443583427052cbbd591e46bf29d2b4664
juan <juanstebanriveros@gmail.com>
2026-08-02 23:50:34 -0500

    Acabado

--- sample_app.py
+++ sample_app.py
@@ -13,7 +13,7 @@ def home():
     except Exception as e:
         db_status = f"Error al conectar a la base de datos: {e}"
 
-    return f"<h1>Bienvenido a mi aplicación Flask</h1><p>{db_status}</p>"
+    return f"<h1>Bienvenido a mi aplicación Flask Ok</h1><p>{db_status}</p>"
 
 if __name__ == "__main__":
-    sample.run(host='0.0.0.0', port=5050 debug=True)
+    sample.run(host='0.0.0.0', port=5050, debug=True)
