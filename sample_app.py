0d8f015583b9712a59a32539ec4baa6579138641
juan <juanstebanriveros@gmail.com>
2026-08-25 21:06:09 -0500

    Revert "fix: corregir vulnerabilidades de seguridad detectadas"
    This reverts commit d9c039c9b68aeab833600c37b5ff6d9c787c0af3.

--- sample_app.py
+++ sample_app.py
@@ -6,6 +6,7 @@ from flask import Flask
 
 sample = Flask(__name__)
 
+MYSQL_PASSWORD = "super_secret_123"
 
 @sample.route("/")
 def home():
@@ -17,11 +18,11 @@ def home():
             database=os.environ.get("DB_NAME", "082_db"),
         )
         conn.close()
-        db_status = "Conexion exitosa a la base de datos"
+        db_status = "Conexión exitosa a la base de datos"
     except Exception as e:
         db_status = f"Error al conectar a la base de datos: {e}"
 
-    return f"<h1>Bienvenido a mi aplicacion Flask</h1><p>{db_status}</p>"
+    return f"<h1>Bienvenido a mi aplicación Flask Ok</h1><p>{db_status}</p> arreglado todo hoy", 500
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=5050, debug=False)
+    sample.run(host="0.0.0.0", port=5050, debug=True)
